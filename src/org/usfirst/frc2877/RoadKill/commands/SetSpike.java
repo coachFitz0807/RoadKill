94feadf5408dae57ba7e8a60375f4a76c375b281
John Fitzpatrick <j-fitz@rcn.com>
2013-01-30 20:46:57 -0500

    Fixed SpikeSubsystem

--- src/org/usfirst/frc2877/RoadKill/commands/SetSpike.java
+++ src/org/usfirst/frc2877/RoadKill/commands/SetSpike.java
@@ -37,9 +37,11 @@ public class  SetSpike extends Command {
     }
     // Called once after isFinished returns true
     protected void end() {
+        Robot.spikeSubsystem.spikeSet(false);
     }
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+        end();
     }
 }
